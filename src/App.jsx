fc9f0ffab03b498854a741e406b0321cce590541
Luis Sanchez <luis.sanchez.dev@gmail.com>
2023-10-21 23:32:39 -0500

    feature: added components full and sections full

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,45 @@
+import { Nav } from "./components";
 import {
   CustomerReviews,
-  Hero,
   Footer,
+  Hero,
   PopularProducts,
   Services,
   SpecialOffer,
   Subscribe,
   SuperQuality,
-} from './sections'
-import Nav from './components/Nav'
+} from "./sections";
 
 const App = () => {
   return (
-    <main  className="relative">
+    <main className='relative'>
       <Nav />
-      <section className="xl-padding-1 wide:padding-r padding-b">
+      <section className='xl:padding-l wide:padding-r padding-b'>
         <Hero />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <PopularProducts />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <SuperQuality />
       </section>
-      <section className="padding-x py-10">
+      <section className='padding-x py-10'>
         <Services />
       </section>
-      <section className="padding">
+      <section className='padding'>
         <SpecialOffer />
       </section>
-      <section className="padding bg-pale-blue">
+      <section className='bg-pale-blue padding'>
         <CustomerReviews />
       </section>
-      <section className="padding-x sm:py-32 py-16 w-full">
+      <section className='padding-x sm:py-32 py-16 w-full'>
         <Subscribe />
       </section>
-      <section className="padding-x padding-y bg-black pb-8">
+      <section className=' bg-black padding-x padding-t pb-8'>
         <Footer />
       </section>
-
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,9 +1,14 @@
-function Services() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import { services } from "../constants";
+import { ServiceCard } from "../components";
 
-export default Services
+const Services = () => {
+  return (
+    <section className='max-container flex justify-center flex-wrap gap-9'>
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
+};
+
+export default Services;
